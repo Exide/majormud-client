3a7bde33747bf80ed2bc43eea51aaf8a49b9c597
Exide <exide@hotmail.com>
2019-05-25 18:55:05 -0700

    Client defaults to the Nocturnal Reign test server.

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Terminal from './terminal.jsx';
 
+const address = 'mud.nocturnalreign.org';
+
 const App = () => (
   <div>
-    <Terminal host="bbs.bearfather.net"/>
+    <Terminal host={address}/>
   </div>
 );
 
