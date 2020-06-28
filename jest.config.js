82df0671cf1a068b22bc1b4d30b9048099718242
Ryan Zander <exide@hotmail.com>
2020-06-27 22:01:29 -0700

    Converted to 2 spaces instead of 4.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: [
-      '/node_modules/',
-      '/dist/'
+    '/node_modules/',
+    '/dist/'
   ]
 };
